0565599fa81527bba1f8020790130965dfaf5329
Ryosuke Hayashi <hayashi@hayashikun.com>
2025-03-10 20:48:40 +0900

    add example

--- example/psi_manual.py
+++ example/psi_manual.py
@@ -4,37 +4,39 @@ from tau.agent import Agent, AgentConfig, AgentToolConfig
 
 load_dotenv()
 
+config: AgentConfig = AgentConfig(
+    model="gpt-4o",
+    prompt="あなたは PSI というプロダクトのサポートエージェントです。"
+           "問い合わせに対して、マニュアルを参照しながら適切な情報を提供してください。最後に、文章を可愛くしてください。",
+    tools=[
+        AgentToolConfig(
+            tool="http_get_json",
+            args={
+                "name": "psi-manual",
+                "description": "PSIのマニュアルを検索できるツールです。検索文字列を与えると、マニュアルの情報を返します。",
+                "url": "https://u4555opmn8.execute-api.us-east-1.amazonaws.com/prod/search",
+                "query_key": "query",
+                "response_key": "body",
+            },
+        ),
+        AgentToolConfig(
+            tool="agent",
+            args={
+                "name": "cute-ai",
+                "description": "文章が与えられると、語尾に「チャピ」をつけて可愛くすることができます",
+                "agent": {
+                    "model": "gpt-4o",
+                    "prompt": "文章が与えられたら、語尾に「チャビ」を付けて可愛くしてください。"
+                              "可愛くした後のテキストだけを出力してください。",
+                    "tools": [],
+                },
+            },
+        ),
+    ]
+)
+
 
 def main():
-    config: AgentConfig = AgentConfig(
-        model="gpt-4o",
-        prompt="あなたは PSI というプロダクトのサポートエージェントです。問い合わせに対して、マニュアルを参照しながら適切な情報を提供してください。最後に、文章を可愛くしてください。",
-        tools=[
-            AgentToolConfig(
-                tool="http_get_json",
-                args={
-                    "name": "psi-manual",
-                    "description": "PSIのマニュアルを検索できるツールです。検索文字列を与えると、マニュアルの情報を返します。",
-                    "url": "https://u4555opmn8.execute-api.us-east-1.amazonaws.com/prod/search",
-                    "query_key": "query",
-                    "response_key": "body",
-                },
-            ),
-            AgentToolConfig(
-                tool="agent",
-                args={
-                    "name": "cute-ai",
-                    "description": "文章が与えられると、語尾に「チャピ」をつけて可愛くすることができます",
-                    "agent": {
-                        "model": "gpt-4o",
-                        "prompt": "文章が与えられたら、語尾に「チャビ」を付けて可愛くしてください。可愛くした後のテキストだけを出力してください。",
-                        "tools": [],
-                    },
-                },
-            ),
-        ]
-    )
-
     agent = Agent(config)
 
     reply = agent.send_message("PSIのラベル管理の手順を教えて")
