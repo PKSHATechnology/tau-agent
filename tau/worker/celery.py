12c4f7d3331d3c783e26de44bc637618b6cd65c4
Ryosuke Hayashi <hayashi@hayashikun.com>
2025-03-13 14:49:45 +0900

    fix

--- tau/worker/celery.py
+++ tau/worker/celery.py
@@ -7,7 +7,9 @@ from celery import Celery
 
 from tau.agent import Agent, AgentConfig
 
-redis_url = os.environ["REDIS_URL"]
+redis_host = os.environ["REDIS_HOST"]
+redis_port = os.environ["REDIS_PORT"]
+redis_url = f"redis://{redis_host}:{redis_port}"
 
 # Create Celery application
 app = Celery(
@@ -26,19 +28,19 @@ app.conf.update(
     broker_connection_retry_on_startup=True,
 )
 
-redis_client = redis.Redis(host=redis_url, db=0)
+redis_client = redis.Redis(host=redis_host, port=redis_port, db=0)
 
 
-@app.task(bind=True, event_loop=True)
-async def start_agent(self, config: dict):
+@app.task(bind=True)
+def start_agent(self, config: dict):
     agent = Agent(AgentConfig(**config))
-    await _set_agent(self.request.id, agent)
+    _set_agent(self.request.id, agent)
     return self.request.id
 
 
-@app.task(event_loop=True)
-async def invoke_agent(agent_id, message: str, callback_url: str):
-    agent = await _get_agent(agent_id)
+@app.task
+def invoke_agent(agent_id, message: str, callback_url: str):
+    agent = _get_agent(agent_id)
     if agent is not None:
         reply = agent.send_message(message)
         print(reply)
@@ -48,27 +50,30 @@ async def invoke_agent(agent_id, message: str, callback_url: str):
                 json={"agent_id": agent_id, "result": "ok", "content": reply}
             )
             response.raise_for_status()
-        await _set_agent(agent_id, agent)
+        _set_agent(agent_id, agent)
     else:
         raise AgentNotFoundError("Agent not found.")
 
 
-@app.task(event_loop=True)
-async def close_agent(task_id):
-    await _del_agent(task_id)
+@app.task
+def close_agent(task_id):
+    _del_agent(task_id)
+
 
 def _redis_keys(agent_id):
     return agent_id + "_config", agent_id + "_messages"
 
-async def _del_agent(agent_id):
-    config_key, messages_key = _redis_keys(agent_id)
-    await redis_client.delete(config_key)
-    await redis_client.delete(messages_key)
 
-async def _get_agent(agent_id):
+def _del_agent(agent_id):
     config_key, messages_key = _redis_keys(agent_id)
-    config_obj = await redis_client.get(config_key)
-    messages_obj = await redis_client.get(messages_key)
+    redis_client.delete(config_key)
+    redis_client.delete(messages_key)
+
+
+def _get_agent(agent_id):
+    config_key, messages_key = _redis_keys(agent_id)
+    config_obj = redis_client.get(config_key)
+    messages_obj = redis_client.get(messages_key)
     if config_obj is not None and messages_obj is not None:
         config = pickle.loads(config_obj)
         messages = pickle.loads(messages_obj)
@@ -78,12 +83,13 @@ async def _get_agent(agent_id):
     else:
         return None
 
-async def _set_agent(agent_id, agent):
+
+def _set_agent(agent_id, agent):
     config_key, messages_key = _redis_keys(agent_id)
     config_obj = pickle.dumps(agent.config)
     messages_obj = pickle.dumps(agent.messages)
-    await redis_client.set(config_key, config_obj)
-    await redis_client.set(messages_key, messages_obj)
+    redis_client.set(config_key, config_obj)
+    redis_client.set(messages_key, messages_obj)
 
 
 class AgentNotFoundError(Exception):
